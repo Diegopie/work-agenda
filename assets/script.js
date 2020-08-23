91f1132515924fc65adbeec8cde0592af5035feb
Diego Hernandez <ishtaracademy@outlook.com>
2020-08-22 18:19:45 -0600

    Added remainder of timeblocks and scaled code so local storage works for all of them. Wrote functionality to dynamically style timeblocks based on static time (for testing)

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,41 @@
 
+$('#currentDay').text(moment().format('dddd, MMMM Mo' ));
+
+let currentHour = moment('12 PM', 'hh A').format('HH');
+console.log(currentHour);
+
+
+console.log(timeBlockTime = $('.row'));
+
+for (t = 0; t < 9; t++) {
+let timeBlockTime = $('.row')[t].children[0].textContent;
+                            // set to i
+let timeBlockMoment = moment(timeBlockTime, "hh A").format('HH');
+
+let intTime = parseInt(currentHour);
+let intMoment = parseInt(timeBlockMoment);
+
+if (intMoment < intTime) {
+    console.log($('.row')[t].children[1]);
+    let addPast = $('.row')[t].children[1];
+    $(addPast).addClass('past');
+} else if (intMoment === intTime) {
+    let addPresent = $('.row')[t].children[1];
+    $(addPresent).addClass('present');
+} else {
+    let addFuture = $('.row')[t].children[1];
+    $(addFuture).addClass('future');
+}
+
+
+}
+
+
+
 // Check Local Storage for Any Values and Place on Screen
 function checkLocal() {
     // Loop Through Every Time-Block
-    for (i = 1; i < 3; i++) {
+    for (i = 1; i < 10; i++) {
     // Variable To Get Value of Current Time-Block Key
     
     var storedAgenda = localStorage.getItem(""+ i +"");
@@ -10,9 +43,15 @@ function checkLocal() {
     // Path to Current Time-Block's Text Area
     var agendaBlock = $("[value='"+i+"']")[0].children[1];
             console.dir(agendaBlock);
+    // Check If There Is Nothing In Local Storage For This Item
+    if (storedAgenda === null) {
+        // If There Is Not, Set Empty Place Holder Text
+        agendaBlock.setAttribute('placeholder', "")
+    } else {
     // Set the Placeholder Text for Current Time-Block To the Value of storedAgenda
     agendaBlock.setAttribute('placeholder', storedAgenda)
        
+    }
     }
 };
 
@@ -48,3 +87,4 @@ $('.container').on('click', function(event){
     // Current Bugs
 
         // time-block will display null if their corelating key has been deleted
+            // Make a conditional in checkLocal if key is not null??
